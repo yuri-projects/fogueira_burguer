7d8064d0a9c8c25892aace58c7a645828724396d
yuri-projects <fideles.projects@gmail.com>
2024-11-07 23:53:03 -0300

    fazendo o deploy do projeto

---
+++ js/dados.js
@@ -0,0 +1,284 @@
+var MENU = {
+    "espetos": [
+        {
+            "id": "01-espeto-frango",
+            "img": "./img/cardapio/espetos/espeto-frango.jpg",
+            "name": "Frango",
+            "dsc": "....",
+            "price": 9.99
+        },
+        {
+            "id": "02-espeto-carne-bacon",
+            "img": "./img/cardapio/espetos/espeto-carne-bacon.jpg",
+            "name": "Carne com bacon",
+            "dsc": "....",
+            "price": 9.99
+        },
+        {
+            "id": "03-espeto-carne",
+            "img": "./img/cardapio/espetos/espeto-carne.jpg",
+            "name": "Carne",
+            "dsc": "...",
+            "price": 9.90
+        },
+        {
+            "id": "04-espeto-linguica",
+            "img": "./img/cardapio/espetos/espeto-linguica.jpg",
+            "name": "Linguiça",
+            "dsc": "...",
+            "price": 9.90
+        }, 
+        
+    ],
+    "burgers": [
+        {
+            "id": "01-classico",
+            "img": "./img/cardapio/burguers/classico.jpg",
+            "name": "Clássico",
+            "dsc": "...",
+            "price": 25.90
+        }, 
+
+        {
+            "id": "02-x-salada",
+            "img": "./img/cardapio/burguers/x-salada.jpg",
+            "name": "X-salada",
+            "dsc": "...",
+            "price": 23.90
+        }, 
+
+        {
+            "id": "03-x-bacon",
+            "img": "./img/cardapio/burguers/x-bacon.jpg",
+            "name": "X-bacon",
+            "dsc": "...",
+            "price": 25.90
+        }, 
+
+        {
+            "id": "04-x-frango",
+            "img": "./img/cardapio/burguers/x-frango.jpg",
+            "name": "X-frango",
+            "dsc": "...",
+            "price": 24.90
+        },
+
+        {
+            "id": "05-x-tudo",
+            "img": "./img/cardapio/burguers/x-tudo.jpg",
+            "name": "X-tudo",
+            "dsc": "...",
+            "price": 28.90
+        }, 
+        {
+            "id": "06-x-calabresa",
+            "img": "./img/cardapio/burguers/x-calabresa.jpg",
+            "name": "X-calabresa",
+            "dsc": "...",
+            "price": 25.90
+        }, 
+        {
+            "id": "07-rustico",
+            "img": "./img/cardapio/burguers/rustico.jpg",
+            "name": "rustico",
+            "dsc": "...",
+            "price": 25.90
+        },
+        {
+            "id": "08-x-bbq",
+            "img": "./img/cardapio/burguers/x-bbq.jpg",
+            "name": "X-barbecue",
+            "dsc": "...",
+            "price": 25.90
+        },
+    ],
+
+    "sobremesas": [
+        {
+            "id": "01-bolo-chocolate",
+            "img": "./img/cardapio/sobremesas/bolo-chocolate.jpg",
+            "name": "Bolo chocolate",
+            "dsc": "...",
+            "price": 12.00
+        }, 
+
+        {
+            "id": "02-brownie",
+            "img": "./img/cardapio/sobremesas/brownie.jpg",
+            "name": "Brownie",
+            "dsc": "...",
+            "price": 15.90
+        }, 
+        {
+            "id": "03-pudim",
+            "img": "./img/cardapio/sobremesas/pudim.jpg",
+            "name": "Pudim",
+            "dsc": "...",
+            "price": 9.90
+        },
+        {
+            "id": "04-mousse-maracuja",
+            "img": "./img/cardapio/sobremesas/mousse-maracuja.jpg",
+            "name": "Mousse maracuja",
+            "dsc": "...",
+            "price": 9.90
+        },
+        {
+            "id": "05-torta-morango",
+            "img": "./img/cardapio/sobremesas/torta-morango.jpg",
+            "name": "Torta morango",
+            "dsc": "...",
+            "price": 12.90
+        },
+        {
+            "id": "06-sorvete",
+            "img": "./img/cardapio/sobremesas/sorvete.jpg",
+            "name": "Sorvete",
+            "dsc": "...",
+            "price": 9.90
+        },
+    ],
+    "bebidas": [
+        {
+            "id": "01-coca-lata",
+            "img": "./img/cardapio/bebidas/coca-lata.png",
+            "name": "Coca lata",
+            "dsc": "...",
+            "price": 8.90
+        }, 
+
+        {
+            "id": "02-guarana-lata",
+            "img": "./img/cardapio/bebidas/guarana-lata.png",
+            "name": "Guarana lata",
+            "dsc": "...",
+            "price": 8.90
+        },
+
+        {
+            "id": "03-agua-sem-gas",
+            "img": "./img/cardapio/bebidas/agua-sem-gas.jpg",
+            "name": "Agua sem gas",
+            "dsc": "...",
+            "price": 5.90
+        },
+
+        {
+            "id": "04-agua-com-gas",
+            "img": "./img/cardapio/bebidas/agua-com-gas.jpg",
+            "name": "Agua com gas",
+            "dsc": "...",
+            "price": 5.90
+        },
+        {
+            "id": "05-suco-laranja",
+            "img": "./img/cardapio/bebidas/suco-laranja.png",
+            "name": "Suco de laranja",
+            "dsc": "...",
+            "price": 7.90
+        },
+
+    ],
+
+    "pizzas": [
+        {
+            "id": "01-frango-rustico",
+            "img": "./img/cardapio/pizzas/frango-rustico.jpg",
+            "name": "Frango rustico",
+            "dsc": "...",
+            "price": 46.90
+        }, 
+        {
+            "id": "02-frango",
+            "img": "./img/cardapio/pizzas/frango.jpg",
+            "name": "Frango",
+            "dsc": "...",
+            "price": 39.90
+        },
+        {
+            "id": "03-marguerita",
+            "img": "./img/cardapio/pizzas/marguerita.jpg",
+            "name": "Marguerita",
+            "dsc": "...",
+            "price": 40.90
+        },
+        {
+            "id": "04-portuguesa",
+            "img": "./img/cardapio/pizzas/portuguesa.jpg",
+            "name": "Portuguesa",
+            "dsc": "...",
+            "price": 40.90
+        }, 
+        {
+            "id": "05-quatro-queijos",
+            "img": "./img/cardapio/pizzas/quatro-queijos.jpg",
+            "name": "Quatro queijos",
+            "dsc": "...",
+            "price": 43.90
+        }, 
+        {
+            "id": "06-calabresa",
+            "img": "./img/cardapio/pizzas/calabresa.jpg",
+            "name": "Calabresa",
+            "dsc": "...",
+            "price": 39.90
+        }, 
+    
+    ],
+
+    "porcoes": [
+        {
+            "id": "01-batata-frita",
+            "img": "./img/cardapio/porcoes/batata-frita.jpg",
+            "name": "Batata frita",
+            "dsc": "...",
+            "price": 20.90
+        },
+        {
+            "id": "02-isca-frango",
+            "img": "./img/cardapio/porcoes/isca-frango.jpg",
+            "name": "Isca de frango",
+            "dsc": "...",
+            "price": 28.90
+        },
+        {
+            "id": "03-porcao-carne",
+            "img": "./img/cardapio/porcoes/porcao-carne.jpg",
+            "name": "Isca de carne",
+            "dsc": "...",
+            "price": 32.90
+        },
+        {
+            "id": "04-isca-peixe",
+            "img": "./img/cardapio/porcoes/isca-frango.jpg",
+            "name": "Isca de frango",
+            "dsc": "...",
+            "price": 35.90
+        },
+        {
+            "id": "05-porcao-calabresa",
+            "img": "./img/cardapio/porcoes/porcao-calabresa.jpg",
+            "name": "Calabresa com cebola",
+            "dsc": "...",
+            "price": 28.90
+        },
+        {
+            "id": "06-bolinho-bacalhau",
+            "img": "./img/cardapio/porcoes/bolinho-bacalhau.jpg",
+            "name": "Bolinho bacalhau",
+            "dsc": "...",
+            "price": 28.90
+        },
+        {
+            "id": "07-porcao-coxinha",
+            "img": "./img/cardapio/porcoes/porcao-coxinha.jpg",
+            "name": "Coxinhas",
+            "dsc": "...",
+            "price": 25.90
+        },
+
+     
+    
+    ],
+  
+}
